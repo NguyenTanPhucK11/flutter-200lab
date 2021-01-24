77e8b71bc3c3436a1628225d1eba04105003b938
NguyenTanPhucK11 <16520954@gm.uit.edu.vn>
2021-01-24 21:51:22 +0700

    day1 - finish

--- lib/screens/Day1/exercise1.dart
+++ lib/screens/Day1/exercise1.dart
@@ -8,10 +8,9 @@ class Exercise1 extends StatefulWidget {
 }
 
 class _Exercise1State extends State<Exercise1> {
-  int segmentedControlValue = 0;
-  int temperature = 0;
-  var temperatureController = TextEditingController();
-  var tempF = TextEditingController();
+  var temperatureC = TextEditingController();
+  var temperatureF = TextEditingController();
+  bool isConvertToF = true;
 
   @override
   Widget build(BuildContext context) {
@@ -22,84 +21,51 @@ class _Exercise1State extends State<Exercise1> {
         body: _buildBody());
   }
 
+  @override
+  void initState() {
+    super.initState();
+  }
+
   Widget _buildBody() {
     return Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            width: 200,
-            child: CupertinoSlidingSegmentedControl(
-                groupValue: segmentedControlValue,
-                backgroundColor: Colors.blue.shade200,
-                children: const <int, Widget>{
-                  0: Text('Độ F'),
-                  1: Text('Độ C'),
-                },
-                onValueChanged: (value) {
-                  setState(() {
-                    segmentedControlValue = value;
-                    temperatureController.text.isEmpty
-                        ? tempF.text = "0"
-                        : tempF.text = value == 0
-                            ? ((int.parse(temperatureController.text) - 32) /
-                                    1.8)
-                                .toStringAsFixed(2)
-                            : (int.parse(temperatureController.text) * 1.8 + 32)
-                                .toStringAsFixed(2);
-                  });
-                }),
+          Text(
+            'Convert temperature from F and C',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
           ),
-          SizedBox(
-            height: 15,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: 95,
-                height: 70,
-                child: TextField(
-                  maxLength: 4,
-                  maxLengthEnforced: true,
-                  textAlignVertical: TextAlignVertical.bottom,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: temperature.toString(),
-                  ),
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  controller: temperatureController,
-                  onChanged: (value) {
-                    value.isEmpty
-                        ? tempF.text = "0"
-                        : tempF.text = segmentedControlValue == 0
-                            ? ((int.parse(value) - 32) / 1.8).toStringAsFixed(2)
-                            : (int.parse(value) * 1.8 + 32).toStringAsFixed(2);
-                  },
-                ),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Container(
-                width: 95,
-                height: 50,
-                child: TextField(
-                  textAlignVertical: TextAlignVertical.top,
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                  controller: tempF,
-                ),
-              ),
-            ],
-          ),
+          _input(true),
+          _input(false),
         ],
+      ),
+    );
+  }
+
+  Widget _input(isConvertToF) {
+    return Container(
+      width: 200,
+      child: TextField(
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          hintText: 'Please enter ' + (isConvertToF ? 'C' : 'F'),
+          labelText: (isConvertToF ? 'C' : 'F') + ' Temperature',
+        ),
+        onChanged: (value) {
+          if (value.isEmpty) {
+            temperatureC.text = '';
+            temperatureF.text = '';
+          } else
+            isConvertToF
+                ? temperatureF.text =
+                    (double.parse(temperatureC.text) * 1.8 + 32)
+                        .toStringAsFixed(2)
+                : temperatureC.text =
+                    ((double.parse(temperatureF.text) - 32) / 1.8)
+                        .toStringAsFixed(2);
+        },
+        controller: isConvertToF ? temperatureC : temperatureF,
       ),
     );
   }
